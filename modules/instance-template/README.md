45f682043056f9bd5f171c8cc8e8bf5935c23d36
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-08-20 12:39:44 -0600

    readme and output updates.
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

---
+++ modules/instance-template/README.md
@@ -0,0 +1,24 @@
+# Terraform Enterprise: High Availability - Instance Template Submodule
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| bootstrap\_token\_id | bootstrap token id | string | n/a | yes |
+| bootstrap\_token\_suffix | bootstrap token suffix | string | n/a | yes |
+| cluster\_endpoint | the cluster endpoint | string | n/a | yes |
+| image\_family | image family | string | n/a | yes |
+| install\_type | type of install - poc or production | string | n/a | yes |
+| ptfe\_subnet | subnet to deploy into | string | n/a | yes |
+| region | The region to install into. | string | n/a | yes |
+| release\_sequence | Replicated release sequence | string | n/a | yes |
+| repl\_data | console | string | n/a | yes |
+| secondary\_machine\_type | Type of machine to use | string | n/a | yes |
+| setup\_token | setup token | string | n/a | yes |
+| boot\_disk\_size | The size of the boot disk to use for the instances | string | `"40"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| secondary\_template |  |
--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,24 @@
-output "replicated_console_password" {
-  value = "${random_pet.console_password.id}"
+output "installer_dashboard_password" {
+  value       = "${random_pet.console_password.id}"
+  description = "The password to access the installer dashboard."
 }
 
-output "replicated_console_url" {
-  value = "https://${google_compute_instance.primary.0.network_interface.0.access_config.0.nat_ip}:8800"
+output "installer_dashboard_url" {
+  value       = "https://${google_compute_instance.primary.0.network_interface.0.access_config.0.nat_ip}:8800"
+  description = "The URL to access the installer dashboard."
 }
-
-/*output "replicated_console_url" {
-  value = "https://${var.frontend_dns}.${substr(data.google_dns_managed_zone.dnszone.dns_name, 0, length(data.google_dns_managed_zone.dnszone.dns_name) - 1)}/dashboard"
-}*/
 
 output "primary_public_ip" {
-  value = "${var.public_ip}"
+  value       = "${var.public_ip}"
+  description = "The Public IP for the load balancer to use."
 }
 
-output "ptfe_endpoint" {
-  value = "https://${var.frontend_dns}.${substr(data.google_dns_managed_zone.dnszone.dns_name, 0, length(data.google_dns_managed_zone.dnszone.dns_name) - 1)}"
+output "application_endpoint" {
+  value       = "https://${var.frontend_dns}.${substr(data.google_dns_managed_zone.dnszone.dns_name, 0, length(data.google_dns_managed_zone.dnszone.dns_name) - 1)}"
+  description = "The URI to access the Terraform Enterprise Application."
 }
 
-output "ptfe_health_check" {
-  value = "https://${var.frontend_dns}.${substr(data.google_dns_managed_zone.dnszone.dns_name, 0, length(data.google_dns_managed_zone.dnszone.dns_name) - 1)}/_health_check"
+output "application_health_check" {
+  value       = "https://${var.frontend_dns}.${substr(data.google_dns_managed_zone.dnszone.dns_name, 0, length(data.google_dns_managed_zone.dnszone.dns_name) - 1)}/_health_check"
+  description = "The URI for the Terraform Enterprise Application health check."
 }
