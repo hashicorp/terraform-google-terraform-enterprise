730d45e1ce425634432ab4bea23a41f016ef3a78
Amy Brown <amy@hashicorp.com>
2019-08-30 11:31:44 -0500

    forgot the value line!

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -36,9 +36,11 @@ module "tfe-beta" {
 }
 
 output "tfe-beta" {
-  application_endpoint         = "${module.tfe-beta.application_endpoint}"
-  application_health_check     = "${module.tfe-beta.application_health_check}"
-  installer_dashboard_password = "${module.tfe-beta.installer_dashboard_password}"
-  installer_dashboard__url     = "${module.tfe-beta.installer_dashboard_url}"
-  primary_public_ip            = "${module.tfe-beta.primary_public_ip}"
+  value = {
+    application_endpoint         = "${module.tfe-beta.application_endpoint}"
+    application_health_check     = "${module.tfe-beta.application_health_check}"
+    installer_dashboard_password = "${module.tfe-beta.installer_dashboard_password}"
+    installer_dashboard__url     = "${module.tfe-beta.installer_dashboard_url}"
+    primary_public_ip            = "${module.tfe-beta.primary_public_ip}"
+  }
 }
