7b3804f144afffef71e6d6c3b6c027f04f0accf6
Amy Brown <amy@hashicorp.com>
2019-10-11 10:40:02 -0500

    jq install fix on rhel
    jq is not in the RHEL repos - added a line to the installer to grab the binary from github.
    
    Also, fixed a typo in the variables description of the weave cidr range.

--- variables.tf
+++ variables.tf
@@ -223,7 +223,7 @@ variable "dns_project" {
 =======
 variable "weave_cidr" {
   type        = "string"
-  description = "Specify a non-standard CIDR range for weave. The default is 172.18.0.0/12"
+  description = "Specify a non-standard CIDR range for weave. The default is 172.18.0.0/16"
   default     = ""
 }
 
