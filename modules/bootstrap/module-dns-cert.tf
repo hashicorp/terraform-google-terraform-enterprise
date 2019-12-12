25f44866a2502cd0d3de8f6720374bd5c33a10a3
Evan Phoenix <evan@phx.io>
2019-12-11 19:24:09 -0800

    Remove old modules

--- modules/bootstrap/module-dns-cert.tf
+++
@@ -1,9 +0,0 @@
-module "dns-cert" {
-  source      = "./modules/dns-cert"
-  domain      = var.domain
-  zone        = var.zone
-  dnszone     = var.dnszone
-  frontenddns = var.frontenddns
-  network_url = module.firewall.google_compute_network_url
-}
-
--- modules/bootstrap/modules/dns-cert/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "dnszone" {
-  type        = "string"
-  description = "name of the managed dns zone"
-}
-
-variable "hostname" {
-  type        = "string"
-  description = "DNS hostname for load balancer, appended with the zone's domain"
-}
