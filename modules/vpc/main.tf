48d0d13683e27a7c6cae3b0fcd98ed87f823172a
Evan Phoenix <evan@phx.io>
2019-12-17 21:41:36 -0800

    Add NAT router to default VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,3 +10,23 @@ resource "google_compute_subnetwork" "tfe_subnet" {
   region        = var.region
   network       = google_compute_network.tfe_vpc.self_link
 }
+
+resource "google_compute_router" "router" {
+  name    = "${var.prefix}router-${var.install_id}"
+  region  = var.region
+  network = google_compute_network.tfe_vpc.self_link
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.prefix}router-nat-${var.install_id}"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
