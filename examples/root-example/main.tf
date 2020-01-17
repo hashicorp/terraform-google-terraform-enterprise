5a4b92b5868542bc6bd317234feefb73a162d9f7
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-01-17 12:48:24 -0500

    Remove obsolete inputs to module

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -23,21 +23,10 @@ provider "template" {
 module "tfe-cluster" {
   source = "../.."
 
-  credentials_file = "auth-file-123456678.json"
-  region           = var.region
-  zone             = "${var.region}-a"
-  project          = var.project
-  domain           = "example.com"
-  dns_zone         = "example"
-  public_ip        = "1.2.3.4"
-  certificate      = "https://www.googleapis.com/compute/v1/project/terraform-test/global/sslCertificates/tfe"
-  ssl_policy       = "tfe-ssl-policy"
-  subnet           = "tfe-subnet"
-  frontend_dns     = "tfe-cluster"
-
-  primary_count   = "3"
-  secondary_count = "2"
-
+  credentials  = "auth-file-123456678.json"
+  dnszone      = "example"
   license_file = "customer.rli"
-}
+  project      = var.project
 
+  region = var.region
+}
