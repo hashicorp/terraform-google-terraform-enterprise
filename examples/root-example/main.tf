f603123259f94af7f865df36d3b92d735c55da03
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-01-17 12:27:55 -0500

    Pin providers to ~> 3.0

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -1,9 +1,13 @@
 provider "google" {
+  version = "~> 3.0"
+
   region  = "${var.region}"
   project = "${var.project}"
 }
 
 provider "google-beta" {
+  version = "~> 3.0"
+
   region  = "${var.region}"
   project = "${var.project}"
 }
