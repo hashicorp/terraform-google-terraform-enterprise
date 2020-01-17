1bc9ce2bba8ddc292aed2bb9e70da898a6487314
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-01-17 12:23:47 -0500

    Move outputs to separate file

---
+++ examples/root-example/outputs.tf
@@ -0,0 +1,12 @@
+output "tfe-cluster" {
+  value = {
+    application_endpoint         = "${module.tfe-cluster.application_endpoint}"
+    application_health_check     = "${module.tfe-cluster.application_health_check}"
+    installer_dashboard_password = "${module.tfe-cluster.installer_dashboard_password}"
+    installer_dashboard__url     = "${module.tfe-cluster.installer_dashboard_url}"
+    primary_public_ip            = "${module.tfe-cluster.primary_public_ip}"
+    encryption_password          = "${module.tfe-cluster.encryption_password}"
+  }
+
+  description = "Cluster information."
+}
