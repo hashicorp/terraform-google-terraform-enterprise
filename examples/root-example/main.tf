b442c73ae13f7c5e3220489335d13ecdbc4461ee
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-01-17 12:23:47 -0500

    Move outputs to separate file

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -28,14 +28,3 @@ module "tfe-cluster" {
 
   license_file = "customer.rli"
 }
-
-output "tfe-cluster" {
-  value = {
-    application_endpoint         = "${module.tfe-cluster.application_endpoint}"
-    application_health_check     = "${module.tfe-cluster.application_health_check}"
-    installer_dashboard_password = "${module.tfe-cluster.installer_dashboard_password}"
-    installer_dashboard__url     = "${module.tfe-cluster.installer_dashboard_url}"
-    primary_public_ip            = "${module.tfe-cluster.primary_public_ip}"
-    encryption_password          = "${module.tfe-beta.encryption_password}"
-  }
-}
