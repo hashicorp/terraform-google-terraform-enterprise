8a94a0a4007ef6518c6fb2efcc3a09e96b4237ea
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-01-17 13:37:04 -0500

    Output tfe-cluster module

--- examples/root-example/outputs.tf
+++ examples/root-example/outputs.tf
@@ -1,13 +1,5 @@
 output "tfe-cluster" {
-  value = {
-    application_endpoint         = module.tfe-cluster.application_endpoint
-    application_health_check     = module.tfe-cluster.application_health_check
-    installer_dashboard_password = module.tfe-cluster.installer_dashboard_password
-    installer_dashboard_url      = module.tfe-cluster.installer_dashboard_url
-    primary_public_ip            = module.tfe-cluster.primary_public_ip
-    encryption_password          = module.tfe-beta.encryption_password
-  }
+  value = module.tfe-cluster
 
   description = "Cluster information."
 }
-
