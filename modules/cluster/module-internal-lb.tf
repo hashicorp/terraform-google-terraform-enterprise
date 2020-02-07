ddc2161a73a80ba823dc9a8930dfe25f00b28ff3
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-02-07 13:51:44 -0500

    Move internal_lb under modules

--- modules/cluster/module-internal-lb.tf
+++ modules/cluster/module-internal-lb.tf
@@ -1,5 +1,5 @@
 module "internal_lb" {
-  source     = "./modules/internal_lb"
+  source     = "./../internal_lb"
   install_id = var.install_id
   prefix     = var.prefix
   region     = var.region
