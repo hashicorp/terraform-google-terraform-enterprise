fb004ee672ae02e027b58d845bc82752231d302b
Matt Whiteley <mwhiteley@fastly.com>
2020-03-05 16:11:19 -0800

    fmt

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -4,9 +4,9 @@ data "google_dns_managed_zone" "dnszone" {
 }
 
 resource "google_dns_record_set" "hostname" {
-  name = "${var.hostname}.${data.google_dns_managed_zone.dnszone.dns_name}"
-  type = "A"
-  ttl  = 300
+  name    = "${var.hostname}.${data.google_dns_managed_zone.dnszone.dns_name}"
+  type    = "A"
+  ttl     = 300
   project = var.project
 
   managed_zone = data.google_dns_managed_zone.dnszone.name
