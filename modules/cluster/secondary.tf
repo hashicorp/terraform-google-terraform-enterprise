feeb55eb741ce583d559ac0838345d9c72167aca
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-03-11 18:58:54 -0400

    Create new secondary templates before destroy old
    This commit works around an error that arises from the
    google_compute_region_instance_group_manager using the template while it
    is destroyed by Terraform.

--- modules/cluster/secondary.tf
+++ modules/cluster/secondary.tf
@@ -34,6 +34,10 @@ resource "google_compute_instance_template" "secondary" {
 
     email = var.secondary_service_account_email
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_compute_region_instance_group_manager" "secondary" {
