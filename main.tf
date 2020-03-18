7f99db749b85381b8e6f51104665314b850f69c1
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-03-18 16:38:56 -0400

    Simplify proxy interface

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "cloud_init" {
 
   application_config = module.application.config
   license_file       = var.cloud_init_license_file
-  proxy_address      = module.proxy.address
+  proxy_address      = module.proxy.address.address
 }
 
 # Configure the TFE primary cluster.
@@ -86,13 +86,12 @@ module "secondary_cluster" {
 module "proxy" {
   source = "./modules/proxy"
 
-  ip_cidr_range            = module.vpc.subnet.ip_cidr_range
-  network                  = module.vpc.vpc_name
-  primaries_instance_group = module.primary_cluster.instance_group.self_link
-  subnetwork               = module.vpc.subnet.name
-  subnetwork_project       = module.vpc.subnet.project
-
-  prefix = var.prefix
+  prefix                                   = var.prefix
+  primary_cluster_instance_group_self_link = module.primary_cluster.instance_group.self_link
+  vpc_network_self_link                    = module.vpc.network_url
+  vpc_subnetwork_ip_cidr_range             = module.vpc.subnet.ip_cidr_range
+  vpc_subnetwork_project                   = module.vpc.subnet.project
+  vpc_subnetwork_self_link                 = module.vpc.subnet.self_link
 }
 
 # Create an SSL certificate to be attached to the external load balancer.
--- modules/proxy/outputs.tf
+++ modules/proxy/outputs.tf
@@ -1,3 +1,5 @@
 output "address" {
-  value = google_compute_address.load_balancer_in.address
+  value = google_compute_address.load_balancer_in
+
+  description = "The IP address of the inbound load balancer."
 }
