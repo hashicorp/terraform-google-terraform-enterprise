fcfb1d064bdf7409c931f1db4bb1a85613aecf58
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-03-19 12:22:41 -0400

    Add prefix to example

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -14,6 +14,12 @@ provider "template" {
   version = "2.1.0"
 }
 
+resource "random_pet" "main" {
+  length    = 2
+  prefix    = "tfe"
+  separator = "-"
+}
+
 module "tfe-cluster" {
   source  = "hashicorp/terraform-enterprise/google"
   version = "0.1.0"
@@ -21,4 +27,6 @@ module "tfe-cluster" {
   cloud_init_license_file   = var.cloud_init_license_file
   dns_managed_zone          = var.dns_managed_zone
   dns_managed_zone_dns_name = var.dns_managed_zone_dns_name
+
+  prefix = "${random_pet.main.id}-"
 }
