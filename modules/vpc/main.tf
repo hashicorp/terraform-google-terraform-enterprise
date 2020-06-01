7967738a1db95993afc10786e78488eee04ecf83
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-06-01 13:58:28 -0400

    Simplify VPC health checks

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -76,6 +76,11 @@ resource "google_compute_global_address" "postgresql" {
 }
 
 locals {
+  all_service_accounts = [
+    var.service_account_primaries_email,
+    var.service_account_primaries_load_balancer_email,
+    var.service_account_secondaries_email,
+  ]
   primaries_service_account = [var.service_account_primaries_email]
   primaries_and_secondaries_service_accounts = [
     var.service_account_primaries_email,
@@ -84,40 +89,22 @@ locals {
   primaries_load_balancer_service_account = [var.service_account_primaries_load_balancer_email]
 }
 
-resource "google_compute_firewall" "health_checks_application" {
-  name    = "${var.prefix}health-checks-application"
+resource "google_compute_firewall" "health_checks" {
+  name    = "${var.prefix}health-checks"
   network = google_compute_network.main.self_link
 
   allow {
     protocol = "tcp"
-
-    ports = [var.application_tcp_port]
   }
-  description             = "Allow ingress of application traffic from the Google health check IP address ranges to the primaries and the secondaries."
+  description             = "Allow ingress of health check traffic from trusted IP address ranges to all compute instances."
   direction               = "INGRESS"
   enable_logging          = true
   source_ranges           = var.health_check_ip_cidr_ranges
-  target_service_accounts = local.primaries_and_secondaries_service_accounts
+  target_service_accounts = local.all_service_accounts
 }
 
-resource "google_compute_firewall" "health_checks_kubernetes" {
-  name    = "${var.prefix}health-checks-kubernetes"
-  network = google_compute_network.main.self_link
-
-  allow {
-    protocol = "tcp"
-
-    ports = [var.kubernetes_tcp_port]
-  }
-  description             = "Allow ingress of Kubernetes traffic from the Google health check IP address ranges to the primaries and internal load balancer."
-  direction               = "INGRESS"
-  enable_logging          = true
-  source_ranges           = var.health_check_ip_cidr_ranges
-  target_service_accounts = [var.service_account_primaries_email, var.service_account_internal_load_balancer_email]
-}
-
-resource "google_compute_firewall" "allow_all_ssh_ui" {
-  name    = "${var.prefix}allow-all-ssh-ui"
+resource "google_compute_firewall" "ssh_ui" {
+  name    = "${var.prefix}ssh-ui"
   network = google_compute_network.main.self_link
 
   allow {
