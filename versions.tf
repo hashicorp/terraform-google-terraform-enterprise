7ef8867d5e1b88d4e0e41ebb260768202abf7f1c
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-06-01 14:38:25 -0400

    Update required providers

--- versions.tf
+++ versions.tf
@@ -3,10 +3,12 @@ terraform {
   required_version = "~> 0.12.17"
 
   required_providers {
-    # 3.2.0 is the earliest version of google which supports google_compute_url_map path_rule
-    google = "~> 3.2"
+    # 3.21.0 is the earliest version of google which supports:
+    # * google_compute_region_target_https_proxy
+    # * google_compute_region_url_map
+    google = "~> 3.21"
     # google-beta is synchronized with google
-    google-beta = "~> 3.2"
+    google-beta = "~> 3.21"
     # 2.1.0 is the earliest version of random and template which is compatible with Terraform 0.12
     random   = "~> 2.1"
     template = "~> 2.1"
