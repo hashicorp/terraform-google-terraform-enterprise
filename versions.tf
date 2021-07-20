976ea937b3013572e4327e21546acfd7469300d8
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-20 15:52:36 -0400

    Remove unused provider requirements from root

--- versions.tf
+++ versions.tf
@@ -4,15 +4,5 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 3.54"
     }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 3.54"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
 }
