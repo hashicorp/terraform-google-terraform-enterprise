520750c76bb15c80467b9bae559481dfdb4cdb48
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-20 16:42:17 -0400

    Add output descriptions to database

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,12 +1,20 @@
 output "netloc" {
   value = google_sql_database_instance.tfe.private_ip_address
+
+  description = "The private IP address of the SQL database instance."
 }
 output "dbname" {
   value = google_sql_database.tfe.name
+
+  description = "The name of the PostgreSQL database."
 }
 output "user" {
   value = google_sql_user.tfe.name
+
+  description = "The name of the PostgreSQL database user."
 }
 output "password" {
   value = google_sql_user.tfe.password
+
+  description = "The password of the PostgreSQL database user."
 }
