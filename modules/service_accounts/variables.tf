b7bfb30c0fc4f2b70eeb2a5abb9c4623db1f67c0
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-21 15:27:59 -0400

    Add variable descriptions, types to service_accounts

--- modules/service_accounts/variables.tf
+++ modules/service_accounts/variables.tf
@@ -1,2 +1,9 @@
-variable "bucket" {}
-variable "namespace" {}
+variable "bucket" {
+  description = "The self link of the storage bucket in which the Terraform Enterprise deployment stores data."
+  type        = string
+}
+
+variable "namespace" {
+  description = "A prefix which will be applied to all resource names."
+  type        = string
+}
