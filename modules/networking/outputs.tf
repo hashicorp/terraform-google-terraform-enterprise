c739eea9c425c5647246343a965323e6f69f31a4
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-21 14:03:32 -0400

    Add output descriptions to networking

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,6 +1,10 @@
 output "subnetwork" {
   value = google_compute_subnetwork.tfe_subnet.self_link
+
+  description = "The self link of the subnetwork."
 }
 output "network" {
   value = google_service_networking_connection.private_vpc_connection.network
+
+  description = "The self link of the network."
 }
