409b99bc4260bfc8ecdc57dd041991f43775dc69
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-21 14:12:09 -0400

    Add output descriptions to object_storage

--- modules/object_storage/outputs.tf
+++ modules/object_storage/outputs.tf
@@ -1,6 +1,10 @@
 output "bucket" {
   value = google_storage_bucket.tfe-bucket.name
+
+  description = "The name of the storage bucket."
 }
 output "project" {
   value = google_storage_bucket.tfe-bucket.project
+
+  description = "The ID of the project in which the storage bucket resides."
 }
