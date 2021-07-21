f15d037ad9fd5a2f1b271338dbf045813338ae84
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-21 15:23:41 -0400

    Add output descriptions to redis

--- modules/redis/outputs.tf
+++ modules/redis/outputs.tf
@@ -1,11 +1,17 @@
 output "host" {
   value = google_redis_instance.redis.host
+
+  description = "The hostname of the Redis endpoint."
 }
 
 output "port" {
   value = google_redis_instance.redis.port
+
+  description = "The port number of the Redis endpoint."
 }
 
 output "password" {
   value = google_redis_instance.redis.auth_string
+
+  description = "The "
 }
