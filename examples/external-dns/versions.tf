d074c9044daba9dc256948719a196b9c0bb94d19
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-22 14:36:59 -0400

    Remove google-beta from external-dns

--- examples/external-dns/versions.tf
+++ examples/external-dns/versions.tf
@@ -7,11 +7,6 @@ terraform {
       version = "~> 3.54"
     }
 
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 3.54"
-    }
-
     tls = {
       source  = "hashicorp/tls"
       version = "~> 3.0"
