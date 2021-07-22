991e3e1c0ec5912ec459c2f7e02e167698ed478d
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-22 11:30:43 -0400

    Add provider requirements to existing-network

--- examples/existing-network/versions.tf
+++ examples/existing-network/versions.tf
@@ -1,4 +1,20 @@
 terraform {
   required_version = ">= 0.14"
 
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.54"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.54"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
+  }
 }
