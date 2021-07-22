8d54fc9cfea0886a0dcdf3b60f8b7c894ac21478
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-22 11:32:35 -0400

    Add variable descriptions, types to external-dns

--- examples/external-dns/variables.tf
+++ examples/external-dns/variables.tf
@@ -1,10 +1,49 @@
-variable "project" {}
-variable "region" {}
-variable "credentials_file" {}
-variable "namespace" {}
-variable "tfe_license_name" {}
-variable "tfe_license_path" {}
-variable "fqdn" {}
-variable "ssl_certificate_name" {}
-variable "dns_zone_name" {}
-variable "dns_create_record" {}
+variable "project" {
+  description = "The GCP project in which resources will be created."
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region in which resources will be created."
+  type        = string
+}
+
+variable "credentials_file" {
+  description = <<-EOD
+  The pathname of the service account key file that will be used to authenticate the google provider against GCP.
+  EOD
+  type        = string
+}
+
+variable "namespace" {
+  description = "A prefix which will be applied to all resource names."
+  type        = string
+}
+
+variable "tfe_license_name" {
+  description = <<-EOD
+  The name that will be assigned to the Replicated license file when it is uploaded to the storage bucket.
+  EOD
+  type        = string
+}
+variable "tfe_license_path" {
+  description = <<-EOD
+  The pathname of the Replicated license file that will be used to authorize the Terraform Enterprise installation.
+  EOD
+  type        = string
+}
+
+variable "fqdn" {
+  description = "The fully qualified domain name which will be assigned to the DNS record."
+  type        = string
+}
+
+variable "dns_zone_name" {
+  description = "The name of the DNS zone in which a record will be created."
+  type        = string
+}
+
+variable "dns_create_record" {
+  description = "A toggle to control the creation of a DNS record for the load balancer IP address."
+  type        = bool
+}
