2fea1e5cb0fe9e8d0ce28221b76d29d5d810d89d
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-07-23 15:49:53 -0400

    Sort PAA variables

--- tests/public-active-active/variables.tf
+++ tests/public-active-active/variables.tf
@@ -11,17 +11,17 @@ variable "iact_subnet_list" {
   type        = list(string)
 }
 
-variable "ssl_certificate_name" {
-  description = <<-EOD
-  The name of an existing SSL certificate which will be used to authenticate connections to the load balancer.
-  EOD
-  type        = string
-}
-
 variable "license_secret" {
   description = <<-EOD
   The Secret Manager secret which comprises the Base64 encoded Replicated license file. The Terraform provider calls
   this value the secret_id and the GCP UI calls it the name.
+  EOD
+  type        = string
+}
+
+variable "ssl_certificate_name" {
+  description = <<-EOD
+  The name of an existing SSL certificate which will be used to authenticate connections to the load balancer.
   EOD
   type        = string
 }
