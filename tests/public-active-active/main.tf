feb4a3e3ed5120f47da5216bfaeb13b1e3d006f8
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-08-10 11:11:18 -0400

    Set iact_subnet_time_limit to 1440

--- tests/public-active-active/main.tf
+++ tests/public-active-active/main.tf
@@ -24,9 +24,10 @@ module "tfe" {
   license_secret       = var.license_secret
   ssl_certificate_name = var.ssl_certificate_name
 
-  iact_subnet_list     = var.iact_subnet_list
-  load_balancer        = "PUBLIC"
-  redis_auth_enabled   = false
-  vm_disk_source_image = data.google_compute_image.ubuntu.self_link
-  vm_machine_type      = "n1-standard-4"
+  iact_subnet_list       = var.iact_subnet_list
+  iact_subnet_time_limit = 1440
+  load_balancer          = "PUBLIC"
+  redis_auth_enabled     = false
+  vm_disk_source_image   = data.google_compute_image.ubuntu.self_link
+  vm_machine_type        = "n1-standard-4"
 }
