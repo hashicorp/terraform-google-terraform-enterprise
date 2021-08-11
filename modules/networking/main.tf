7f6c24659db30989930333786fbf282d5283471d
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-08-11 16:38:34 -0400

    Increase min_ports_per_vm to 4096

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -31,6 +31,8 @@ resource "google_compute_router_nat" "nat" {
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
+  min_ports_per_vm = 4096
+
   log_config {
     enable = true
     filter = "ERRORS_ONLY"
