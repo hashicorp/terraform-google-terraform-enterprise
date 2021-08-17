40edafae12bc7c7ac35b17a08f3408ffc6b38c43
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-08-17 13:17:33 -0400

    Add required test outputs

--- tests/private-active-active/outputs.tf
+++ tests/private-active-active/outputs.tf
@@ -1,5 +1,35 @@
+output "health_check_url" {
+  value = module.tfe.health_check_url
+
+  description = "The URL of the Terraform Enterprise health check endpoint."
+}
+
+output "iact_url" {
+  value = module.tfe.iact_url
+
+  description = "The URL of the Terraform Enterprise IACT."
+}
+
+output "initial_admin_user_url" {
+  value = module.tfe.initial_admin_user_url
+
+  description = "The URL of the Terraform Enterprise initial admin user."
+}
+
+output "proxy_instance_name" {
+  value = google_compute_instance.http_proxy.name
+
+  description = "The name of the HTTP proxy compute instance."
+}
+
 output "tfe" {
   value = module.tfe
 
   description = "The Terraform Enterprise deployment."
+}
+
+output "tfe_url" {
+  value = module.tfe.url
+
+  description = "The URL of Terraform Enterprise."
 }
