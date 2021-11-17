aa42696b55f6b10e1fc82f726900e7df4d3d7cc4
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-11-17 14:24:43 -0500

    Configure google, tfe providers

--- tests/public-active-active/data.tf
+++ tests/public-active-active/data.tf
@@ -1,6 +1,6 @@
 data "tfe_outputs" "base" {
-  organization = var.tfe_organization
-  workspace    = var.tfe_workspace
+  organization = var.tfe.organization
+  workspace    = var.tfe.workspace
 }
 
 data "google_compute_image" "ubuntu" {
--- tests/public-active-active/variables.tf
+++ tests/public-active-active/variables.tf
@@ -1,3 +1,13 @@
+variable "google" {
+  description = "Attributes of the Google Cloud account which will host the test infrastructure."
+  type = object({
+    credentials = string
+    project     = string
+    region      = string
+    zone        = string
+  })
+}
+
 variable "iact_subnet_list" {
   default     = []
   description = <<-EOD
@@ -7,12 +17,12 @@ variable "iact_subnet_list" {
   type        = list(string)
 }
 
-variable "tfe_organization" {
-  description = "The name of the TFE organization which contains the base workspace."
-  type        = string
-}
-
-variable "tfe_workspace" {
-  description = "The name of the TFE workspace which contains the base state."
-  type        = string
+variable "tfe" {
+  description = "Attributes of the Terraform Enterprise instance which manages the base infrastructure."
+  type = object({
+    hostname     = string
+    organization = string
+    token        = string
+    workspace    = string
+  })
 }
