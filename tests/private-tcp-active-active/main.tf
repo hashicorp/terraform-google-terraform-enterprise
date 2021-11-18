c3230a6e71c7ecac2e973639d6c936867842b6b9
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-11-18 13:27:47 -0500

    Update PTAA test for transient workspaces

--- tests/private-tcp-active-active/main.tf
+++ tests/private-tcp-active-active/main.tf
@@ -19,13 +19,13 @@ resource "google_project_iam_member" "log_writer" {
 resource "google_secret_manager_secret_iam_member" "http_proxy_certificate" {
   member    = local.http_proxy_account
   role      = "roles/secretmanager.secretAccessor"
-  secret_id = var.ca_certificate_secret_id
+  secret_id = data.tfe_outputs.base.values.ca_certificate_secret_id
 }
 
 resource "google_secret_manager_secret_iam_member" "http_proxy_private_key" {
   member    = local.http_proxy_account
   role      = "roles/secretmanager.secretAccessor"
-  secret_id = var.ca_private_key_secret_id
+  secret_id = data.tfe_outputs.base.values.ca_private_key_secret_id
 }
 
 resource "google_compute_firewall" "http_proxy" {
@@ -64,17 +64,6 @@ resource "google_compute_firewall" "ssh" {
   }
 }
 
-data "google_compute_image" "rhel" {
-  name    = "rhel-7-v20200403"
-  project = "gce-uefi-images"
-}
-
-data "google_compute_image" "ubuntu" {
-  name = "ubuntu-2004-focal-v20210119a"
-
-  project = "ubuntu-os-cloud"
-}
-
 resource "google_compute_instance" "http_proxy" {
   boot_disk {
     initialize_params {
@@ -89,9 +78,9 @@ resource "google_compute_instance" "http_proxy" {
   metadata_startup_script = templatefile(
     "${path.module}/templates/startup.sh.tpl",
     {
-      certificate_secret_id = var.ca_certificate_secret_id
+      certificate_secret_id = data.tfe_outputs.base.values.ca_certificate_secret_id
       http_proxy_port       = local.http_proxy_port
-      private_key_secret_id = var.ca_private_key_secret_id
+      private_key_secret_id = data.tfe_outputs.base.values.ca_private_key_secret_id
     }
   )
 
@@ -112,21 +101,21 @@ resource "google_compute_instance" "http_proxy" {
 module "tfe" {
   source = "../.."
 
-  dns_zone_name  = var.cloud_dns_name
-  fqdn           = "private-tcp-active-active.${var.cloud_dns_domain}"
+  dns_zone_name  = data.google_dns_managed_zone.main.name
+  fqdn           = "private-tcp-active-active.${data.google_dns_managed_zone.main.dns_name}"
   namespace      = random_pet.main.id
   node_count     = 2
-  license_secret = var.license_secret_id
+  license_secret = data.tfe_outputs.base.values.license_secret_id
   labels         = local.labels
 
-  ca_certificate_secret  = var.ca_certificate_secret_id
+  ca_certificate_secret  = data.tfe_outputs.base.values.ca_certificate_secret_id
   iact_subnet_list       = ["${google_compute_instance.http_proxy.network_interface[0].network_ip}/32"]
   iact_subnet_time_limit = 1440
   load_balancer          = "PRIVATE_TCP"
   proxy_ip               = "${google_compute_instance.http_proxy.network_interface[0].network_ip}:${local.http_proxy_port}"
   redis_auth_enabled     = true
-  ssl_certificate_secret = var.ssl_certificate_secret_id
-  ssl_private_key_secret = var.ssl_private_key_secret_id
+  ssl_certificate_secret = data.tfe_outputs.base.values.wildcard_ssl_certificate_secret_id
+  ssl_private_key_secret = data.tfe_outputs.base.values.wildcard_ssl_private_key_secret_id
   vm_disk_source_image   = data.google_compute_image.rhel.self_link
   vm_machine_type        = "n1-standard-32"
 }
--- tests/private-tcp-active-active/variables.tf
+++ tests/private-tcp-active-active/variables.tf
@@ -1,43 +1,19 @@
-variable "ca_certificate_secret_id" {
-  description = <<-EOD
-  The identity of the Secret Manager secret which comprises the Base64 encoded PEM CA certificate for Terraform
-  Enterprise and  mitmproxy.
-  EOD
-  type        = string
+variable "google" {
+  description = "Attributes of the Google Cloud account which will host the test infrastructure."
+  type = object({
+    credentials = string
+    project     = string
+    region      = string
+    zone        = string
+  })
 }
 
-variable "ca_private_key_secret_id" {
-  description = <<-EOD
-  The identity of the Secret Manager secret which comprises the Base64 encoded PEM CA private key for mitmproxy.
-  EOD
-  type        = string
-}
-
-variable "cloud_dns_domain" {
-  description = "The domain of the Cloud DNS zone in which a record will be created."
-  type        = string
-}
-
-variable "cloud_dns_name" {
-  description = "The name of the Cloud DNS zone in which a record will be created."
-  type        = string
-}
-
-variable "license_secret_id" {
-  description = "The identity of the Secret Manager secret which comprises the Base64 encoded Replicated license file."
-  type        = string
-}
-
-variable "ssl_certificate_secret_id" {
-  description = <<-EOD
-  The identity of the Secret Manager secret which comprises the Base64 encoded PEM certificate for Terraform Enterprise.
-  EOD
-  type        = string
-}
-
-variable "ssl_private_key_secret_id" {
-  description = <<-EOD
-  The identity of the Secret Manager secret which comprises the Base64 encoded PEM private key for Terraform Enterprise.
-  EOD
-  type        = string
+variable "tfe" {
+  description = "Attributes of the Terraform Enterprise instance which manages the base infrastructure."
+  type = object({
+    hostname     = string
+    organization = string
+    token        = string
+    workspace    = string
+  })
 }
