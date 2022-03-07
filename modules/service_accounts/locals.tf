40ad5fa4358de3f270d499b1453541e9aa597f18
John Kerry <john.kerry@hashicorp.com>
2022-03-07 16:09:50 -0500

    Existing service account (#140)

--- modules/service_accounts/locals.tf
+++ modules/service_accounts/locals.tf
@@ -1,3 +1,5 @@
 locals {
-  member = "serviceAccount:${google_service_account.main.email}"
+  service_account = var.existing_service_account_id == null ? google_service_account.main[0] : data.google_service_account.main[0]
+
+  member = "serviceAccount:${local.service_account.email}"
 }
