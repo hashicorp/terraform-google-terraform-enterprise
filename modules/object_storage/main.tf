d8348cd152c9a44ff586b13be1edb18ac7bc53b9
John Kerry <john.kerry@hashicorp.com>
2022-03-14 15:37:02 -0400

    enforcing uniform bucket level access

--- modules/object_storage/main.tf
+++ modules/object_storage/main.tf
@@ -5,7 +5,7 @@ resource "random_pet" "gcs" {
 resource "google_storage_bucket" "tfe" {
   name     = "${var.namespace}-storage-${random_pet.gcs.id}"
   location = "us"
-
+  uniform_bucket_level_access = true
   force_destroy = true
   labels        = var.labels
 }
