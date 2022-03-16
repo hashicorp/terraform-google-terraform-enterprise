f268238ff7577dcfa2be76e53a34a24c8074d02e
John Kerry <john.kerry@hashicorp.com>
2022-03-16 18:02:47 -0400

    changing the default reserve ip range for east4 installation

--- tests/private-tcp-active-active/main.tf
+++ tests/private-tcp-active-active/main.tf
@@ -21,22 +21,22 @@ module "test_proxy" {
 module "tfe" {
   source = "../.."
 
-  dns_zone_name               = data.google_dns_managed_zone.main.name
-  fqdn                        = "private-tcp-active-active.${data.google_dns_managed_zone.main.dns_name}"
-  namespace                   = random_pet.main.id
-  existing_service_account_id = var.google.service_account
-  node_count                  = 2
-  license_secret              = data.tfe_outputs.base.values.license_secret_id
-  labels                      = local.labels
-
-  ca_certificate_secret    = data.tfe_outputs.base.values.ca_certificate_secret_id
-  iact_subnet_list         = ["${module.test_proxy.compute_instance.network_interface[0].network_ip}/32"]
-  iact_subnet_time_limit   = 1440
-  load_balancer            = "PRIVATE_TCP"
-  http_proxy_uri_authority = module.test_proxy.uri_authority
-  redis_auth_enabled       = true
-  ssl_certificate_secret   = data.tfe_outputs.base.values.wildcard_ssl_certificate_secret_id
-  ssl_private_key_secret   = data.tfe_outputs.base.values.wildcard_ssl_private_key_secret_id
-  vm_disk_source_image     = data.google_compute_image.rhel.self_link
-  vm_machine_type          = "n1-standard-32"
+  dns_zone_name                   = data.google_dns_managed_zone.main.name
+  fqdn                            = "private-tcp-active-active.${data.google_dns_managed_zone.main.dns_name}"
+  namespace                       = random_pet.main.id
+  existing_service_account_id     = var.google.service_account
+  node_count                      = 2
+  license_secret                  = data.tfe_outputs.base.values.license_secret_id
+  labels                          = local.labels
+  networking_reserve_subnet_range = "10.2.0.0/16"
+  ca_certificate_secret           = data.tfe_outputs.base.values.ca_certificate_secret_id
+  iact_subnet_list                = ["${module.test_proxy.compute_instance.network_interface[0].network_ip}/32"]
+  iact_subnet_time_limit          = 1440
+  load_balancer                   = "PRIVATE_TCP"
+  http_proxy_uri_authority        = module.test_proxy.uri_authority
+  redis_auth_enabled              = true
+  ssl_certificate_secret          = data.tfe_outputs.base.values.wildcard_ssl_certificate_secret_id
+  ssl_private_key_secret          = data.tfe_outputs.base.values.wildcard_ssl_private_key_secret_id
+  vm_disk_source_image            = data.google_compute_image.rhel.self_link
+  vm_machine_type                 = "n1-standard-32"
 }
