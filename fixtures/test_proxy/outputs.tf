17c67e2d9d7c4c9db1c6d39540ca9c82b0dae52f
John Kerry <john.kerry@hashicorp.com>
2022-04-05 11:00:41 -0400

    adding output descriptions for proxy port and ip

--- fixtures/test_proxy/outputs.tf
+++ fixtures/test_proxy/outputs.tf
@@ -6,8 +6,12 @@ output "compute_instance" {
 
 output "proxy_ip" {
   value = google_compute_instance.proxy.network_interface[0].network_ip
+
+  description = "The listening port for redis."
 }
 
 output "proxy_port" {
   value = local.http_port
+
+  description = "The listening ip for redis."
 }
