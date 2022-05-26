2dcf42adff5aa82da32e88b0718c6c5d95acf9e0
Nathan Lacey <nathanl79@live.com>
2022-05-26 09:54:43 -0700

    Testing PostgreSQL v13 on GCP

--- tests/private-active-active/main.tf
+++ tests/private-active-active/main.tf
@@ -36,4 +36,5 @@ module "tfe" {
   redis_auth_enabled          = true
   vm_disk_source_image        = data.google_compute_image.rhel.self_link
   vm_machine_type             = "n1-standard-16"
+  database_version            = "POSTGRES_13"
 }
