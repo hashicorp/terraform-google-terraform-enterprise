e48ba1faf2548a9bf8b1d91058b3ba4dcf93c91d
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-05-31 13:09:58 -0400

    lint fix

--- examples/existing-network/versions.tf
+++ examples/existing-network/versions.tf
@@ -11,5 +11,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.54"
+    }
   }
 }
