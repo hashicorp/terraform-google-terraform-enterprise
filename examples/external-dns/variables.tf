32e507024e2c450a271ed61470a7b46a44cf9a51
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-06-03 10:45:34 -0400

    external-dns (#162)
    * external-dns

--- examples/external-dns/variables.tf
+++ examples/external-dns/variables.tf
@@ -1,18 +1,3 @@
-variable "namespace" {
-  description = "A prefix which will be applied to all resource names."
-  type        = string
-}
-
-variable "license_file" {
-  type        = string
-  description = "The local path to the Terraform Enterprise license."
-}
-
-variable "fqdn" {
-  description = "The fully qualified domain name which will be assigned to the DNS record."
-  type        = string
-}
-
 variable "dns_zone_name" {
   description = "The name of the DNS zone in which a record will be created."
   type        = string
@@ -21,4 +6,37 @@ variable "dns_zone_name" {
 variable "dns_create_record" {
   description = "A toggle to control the creation of a DNS record for the load balancer IP address."
   type        = bool
+}
+
+variable "fqdn" {
+  description = "The fully qualified domain name which will be assigned to the DNS record."
+  type        = string
+}
+
+variable "google" {
+  description = "Attributes of the Google Cloud account which will host the test infrastructure."
+  type = object({
+    credentials     = string
+    project         = string
+    region          = string
+    zone            = string
+    service_account = string
+  })
+}
+
+variable "license_file" {
+  type        = string
+  description = "The local path to the Terraform Enterprise license."
+}
+
+variable "namespace" {
+  description = "A prefix which will be applied to all resource names."
+  type        = string
+}
+
+variable "ssl_certificate_name" {
+  description = <<-EOD
+  The name of an existing SSL certificate which will be used to authenticate connections to the load balancer.
+  EOD
+  type        = string
 }
