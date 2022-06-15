46c39848eb7fde2cdb2c1e3a36474aca77e0d104
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-06-15 17:28:03 -0400

    add license secret fixture

--- tests/standalone-external-rhel8-worker/variables.tf
+++ tests/standalone-external-rhel8-worker/variables.tf
@@ -3,6 +3,12 @@ variable "existing_service_account_id" {
   description = "The id of the logging service account to use for compute resources deployed."
 }
 
+variable "license_file" {
+  default     = null
+  type        = string
+  description = "The local path to the Terraform Enterprise license to be provided by CI."
+}
+
 variable "tfe" {
   description = "Attributes of the Terraform Enterprise instance which manages the base infrastructure."
   type = object({
@@ -11,4 +17,10 @@ variable "tfe" {
     token        = string
     workspace    = string
   })
+}
+
+variable "tfe_license_secret_id" {
+  default     = null
+  type        = string
+  description = "The Secrets Manager secret ARN under which the Base64 encoded Terraform Enterprise license is stored."
 }
