e38b1df716f46d559eab8fd209e484777f800d18
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-07-08 13:02:50 -0400

    provider update

--- tests/public-active-active/versions.tf
+++ tests/public-active-active/versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = ">= 0.14"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.54"
+      version = "~> 4.27.0"
     }
 
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.54"
+      version = "~> 4.27.0"
     }
 
     random = {
