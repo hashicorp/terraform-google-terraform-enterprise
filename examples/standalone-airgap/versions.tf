a946445f67c685aa8ac190885e0d5aa475415084
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-08-04 11:46:26 -0400

    update

--- examples/standalone-airgap/versions.tf
+++ examples/standalone-airgap/versions.tf
@@ -1,10 +1,3 @@
 terraform {
   required_version = ">= 0.14"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.54"
-    }
-  }
 }
--- examples/standalone-mounted/versions.tf
+++ examples/standalone-mounted/versions.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.54"
-    }
-
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
