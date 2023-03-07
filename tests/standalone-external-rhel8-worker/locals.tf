f06fbfe1179cb30b8076d4376dcfbfdd470cff92
anniehedgpeth <annie@hashicorp.com>
2023-03-07 13:44:38 -0700

    add logic to define repo location by project

--- tests/standalone-external-rhel8-worker/locals.tf
+++ tests/standalone-external-rhel8-worker/locals.tf
@@ -2,8 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 locals {
-  repository_location = "us-west1"
-  repository_name     = "terraform-build-worker"
-  ssh_user            = "ubuntu"
-  enable_ssh_config   = length(var.license_file) > 0 ? 1 : 0
+  repository_name   = "terraform-build-worker"
+  ssh_user          = "ubuntu"
+  enable_ssh_config = length(var.license_file) > 0 ? 1 : 0
+
+  project_regions = {
+    "hc-50fbe27799384c96925f18084d7" = "us-west1"
+    "tfe-modules-ci-001"             = "us-east4"
+  }
+  repository_location = project_regions[data.google_project.project.project_id]
 }
