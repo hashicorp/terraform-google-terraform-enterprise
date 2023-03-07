26173896bb6a2419b49c835641cfc4bb6afbc71b
anniehedgpeth <annie@hashicorp.com>
2023-03-07 14:23:16 -0700

    use project names

--- tests/standalone-external-rhel8-worker/locals.tf
+++ tests/standalone-external-rhel8-worker/locals.tf
@@ -7,8 +7,8 @@ locals {
   enable_ssh_config = length(var.license_file) > 0 ? 1 : 0
 
   project_regions = {
-    "hc-50fbe27799384c96925f18084d7" = "us-west1"
-    "tfe-modules-ci-001"             = "us-east4"
+    "ptfe-replicated-ci" = "us-west1"
+    "tfe-modules-ci"     = "us-east4"
   }
   repository_location = local.project_regions[data.google_project.project.project_id]
 }
