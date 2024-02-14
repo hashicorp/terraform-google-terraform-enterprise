84c0251c8e450ff68ce483f2ed9bf535a526d7e4
Miguel Hernández <miguel.hernandez@hashicorp.com>
2024-02-14 12:40:03 +0100

    try to fix it

--- main.tf
+++ main.tf
@@ -375,6 +375,12 @@ module "vm_mig" {
       type = "OPPORTUNISTIC"
       instance_redistribution_type = "NONE"
       minimal_action = var.is_replicated_deployment ? "RESTART" : "REPLACE"
+      max_surge_fixed = null
+      max_surge_percent = null
+      max_unavailable_fixed = null
+      max_unavailable_percent = null
+      min_ready_sec = null
+      replacement_method = null
     }
   ] : []
 }
