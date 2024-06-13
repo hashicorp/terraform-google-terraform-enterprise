a8c7aba26530f3645d35285d335dd0c121169809
Miguel Hernández <miguel.hernandez@hashicorp.com>
2024-06-13 16:53:25 +0200

    remove deletion_policy in google_service_networking_connection

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -101,6 +101,5 @@ resource "google_compute_global_address" "private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.tfe_vpc.self_link
   service                 = "servicenetworking.googleapis.com"
-  deletion_policy         = "ABANDON"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
