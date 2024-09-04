c3907ee73d726130d126860a8c0f34556eb82b4a
Nikolas Rieble <nikolas@rieble.com>
2024-09-04 20:56:15 +0200

    Chore: Remove module tests

--- tests/private-tcp-active-active/providers.tf
+++
@@ -1,7 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-provider "tfe" {
-  hostname = try(var.tfe.hostname, var.tfe_hostname)
-  token    = try(var.tfe.token, var.tfe_token)
-}
